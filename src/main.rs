69652cf03494822b8a228d0d8077fdc4fc183724
Issawat <actiondeveloper1998@gmail.com>
2022-12-26 11:37:41 +0700

    feat: read text file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,4 +10,8 @@ fn main() {
 
     println!("Searching for {query_string}");
     println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("with text content:\n {contents}");
 }
