474b6a14274691e99c64ea68469691a97a98d202
Issawat <actiondeveloper1998@gmail.com>
2022-12-26 13:18:53 +0700

    feat: extract config and runner

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let executable_path = &args[0];
-    let query_string = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    dbg!(executable_path);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
+}
 
-    println!("Searching for {query_string}");
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("with text content:\n {contents}");
+    Ok(())
+}
+
+struct Config {
+    query_string: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Required arguments are missing!");
+        }
+
+        let query_string = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config {
+            query_string,
+            file_path,
+        })
+    }
 }
